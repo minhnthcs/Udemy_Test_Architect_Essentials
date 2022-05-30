f249a7a900badfb537b186b34bbe6f5252d77dbe
mason <mason@arevolabs.com>
2022-05-30 10:27:40 +0700

    write java objects to json files to input to api automatio test later

--- src/main/java/api_src/jsonToJava.java
+++ src/main/java/api_src/jsonToJava.java
@@ -1,25 +1,35 @@
 package api_src;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class jsonToJava {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-//        System.out.println(forName("com.mysql.cj.jdbc.Driver"));
         Connection conn=null;
+        //Create an ArrayList to store all customer records
+        ArrayList<CustomerDetails> customers = new ArrayList<CustomerDetails>();
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","mason");
 
         //object of statement class will help us to execute queries
         Statement st=conn.createStatement();
-        ResultSet rs = st.executeQuery("select * from CustomerInfo where location = \"Asia\" and PurchasedDate=curdate() limit 1");
+        ResultSet rs = st.executeQuery("select * from CustomerInfo where location = \"Asia\" and PurchasedDate=curdate()");
 //        rs.next(); // setting pointer to particular row
         while (rs.next()){
             CustomerDetails customer = new CustomerDetails();
-//            System.out.println(rs.getString(1));
             customer.setCourseName(rs.getString(1));
             customer.setPurchasedDate(rs.getString(2));
             customer.setAmount(rs.getInt(3));
             customer.setLocation(rs.getString(4));
-            System.out.println(customer.getAmount());
+            customers.add(customer);
+        }
+
+        for (int i = 0; i < customers.size(); i++) {
+            ObjectMapper ojm = new ObjectMapper();
+            ojm.writeValue(new File("C:\\Users\\DELL\\Documents\\playground\\JsonJava\\customerInfo" + i + ".json"), customers.get(i));
         }
         conn.close();
     }
